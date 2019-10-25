48bd83430d0c964018be06dceac8f1e19f95c1dc
vannyle <vannnyle@gmail.com>
2019-10-26 01:49:25 +0700

    Доработки по Javascript

--- js/popup.js
+++ js/popup.js
@@ -1,32 +1,45 @@
 var link = document.querySelector(".button-hotel-searching");
 var popup = document.querySelector(".search-form");
-  popup.classList.add("modal-close");
+popup.classList.add("modal-close");
 
 var form = document.querySelector(".search-information");
 var arrival = form.querySelector("[name=arrival]");
 var departure = form.querySelector("[name=departure]");
+var adult = form.querySelector("[name=adult]");
+var children = form.querySelector("[name=children]");
 
+var adultCount = "";
+var childrenCount = "";
 
+try {
+  adultCount = localStorage.getItem("adult");
+  childrenCount = localStorage.getItem("children");
+} catch (e) {
+
+}
+
+adult.value = adultCount;
+children.value = childrenCount;
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (popup.classList.contains("modal-close")) {
+    popup.classList.remove("modal-close");
     popup.classList.add("modal-shown");
-    setTimeout(function () {
-      arrival.focus();
-    }, 500);
   } else {
-    popup.classList.remove("modal-shown")
+    popup.classList.remove("modal-shown");
     popup.classList.add("modal-close");
   }
 });
 
-
-
 form.addEventListener("submit", function (evt) {
-  if (!arrival.value || !departure.value) {
   evt.preventDefault();
-  popup.classList.remove("modal-error");
-  popup.offsetWidth = popup.offsetWidth;
-  popup.classList.add("modal-error");
+  if (!arrival.value || !departure.value) {
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    localStorage.setItem("adult", adult.value);
+    localStorage.setItem("children", children.value);
+    form.submit();
   }
 });
